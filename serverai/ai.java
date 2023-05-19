4ac8a0d251d20c149cb5a6502e183aabd379b568
РомаН <119739400+romausnul@users.noreply.github.com>
2023-05-19 08:49:03 +0300

    Add files via upload

--- serverai/ai.java
+++ serverai/ai.java
@@ -5,8 +5,10 @@ public class RedirectServlet extends HttpServlet {
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
   throws ServletException, IOException {
 
-      System.out.println("----- ТУТ ДОЛЖЕН БЫТЬ СЛАВА НО ЕГО ТУТ НЕТ ---------");
-      resp.setStatus(HttpServletResponse.SC_FOUND);//302
-      resp.setHeader("Location", "http://rmclouddata:101/server/api/ai");
+      System.out.println("----- ТУТ ДОЛЖЕН БЫТЬ СЛАВА НО ЕГО ТУТ НЕТ ---------"); // это вообще что такое я не знаю но знаю 
+      resp.setStatus(HttpServletResponse.SC_FOUND); // тип подключения 
+      resp.setHeader("Location", "http://rmclouddata:101/server/api/ai"); // расположение нейронки 
+      RedirectServlet("rmclouddata:1986/"); // основной сервер 
+      resp.server("server.mamon.host"); // сайт сервера кста рабоатет вроде или нет 
   }
 }
